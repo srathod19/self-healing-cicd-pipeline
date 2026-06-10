0618a8ffb3f0c7f88a26833cba0b676f5cc3a0cf
Sachin Rathod <61534295+srathod19@users.noreply.github.com>
2026-06-10 17:54:29 +0530

    model changed

--- agent/nodes.py
+++ agent/nodes.py
@@ -13,7 +13,7 @@ GEMINI_KEY = os.getenv("GOOGLE_API_KEY", "")
 SLACK_WEBHOOK = os.getenv("SLACK_WEBHOOK_URL", "")
 
 llm = ChatGoogleGenerativeAI(
-    model="gemini-1.5-flash",
+    model="gemini-2.5-flash",
     google_api_key=GEMINI_KEY,
     temperature=0.2,
 )
